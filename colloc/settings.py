fe3d91155461728d4430e2798d3a62fbef101f74
collocmanager-coder <colloc.manager@gmail.com>
2025-11-12 01:08:11 +0300

    autre modif

--- colloc/settings.py
+++ colloc/settings.py
@@ -35,9 +35,9 @@ ALLOWED_HOSTS = ['colloc-manager.onrender.com']
 ###### Hum askip c'est pas bon de mettre les valeurs la en dur mais bon moi je vais les mettre
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-
+EMAIL_PORT = 465
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = True
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
